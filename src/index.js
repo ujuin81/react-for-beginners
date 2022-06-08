3eda7af1c5e3a8f4526eebb79c67dfa883140eb6
하보영 <@IT->
2022-06-08 14:13:58 +0900

    #6.1 useEffect

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 // import './styles.css'; // 일반적인 방식이지만 여기서는 사용하지 않을 것, 모든곳에 css가 적용되길 원하지 않음
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
