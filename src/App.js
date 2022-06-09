a45211e7326b9c8f4e3ed0ac8b57412f6283c467
하보영 <@IT->
2022-06-09 15:28:04 +0900

    #7.2 Coin Tracker - challenge

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import { useState, useEffect } from "react";
 
+function Select({ coins }) {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const onChangeOption = (event) => {
+    setSelectedIndex(event.target.selectedIndex);
+  };
+
+  const [money, setMoney] = useState(0);
+  const [calculateCoin, setCalculateCoin] = useState(0);
+  const onChangeMoney = (event) => setMoney(event.target.value);
+
+  const calculate = () =>
+    setCalculateCoin(money / coins[selectedIndex].quotes.USD.price);
+
+  console.log("Select");
+  return (
+    <div>
+      $
+      <input
+        onChange={onChangeMoney}
+        value={money}
+        type="text"
+        placeholder="input money"
+      />
+      ->
+      <select onChange={onChangeOption}>
+        {coins.map((item) => (
+          <option key={item.id} value={item.quotes.USD.price}>
+            {item.name} ({item.symbol}) : ${item.quotes.USD.price} USD
+          </option>
+        ))}
+      </select>
+      <button onClick={calculate}>Calculate!</button>
+      <div>
+        exchange coin => {calculateCoin} {coins[selectedIndex].symbol}
+      </div>
+    </div>
+  );
+}
+
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
 
   console.log("App load");
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=20")
       .then((response) => response.json())
       .then((json) => {
         setCoins(json);
@@ -16,17 +55,7 @@ function App() {
   return (
     <div>
       <h1>The Coins! {loading ? "" : `(${coins.length})`} </h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((item) => (
-            <option key={item.id}>
-              {item.name} ({item.symbol}) : ${item.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
+      {loading ? <strong>Loading...</strong> : <Select coins={coins} />}
     </div>
   );
 }
