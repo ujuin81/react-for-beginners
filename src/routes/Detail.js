406e708b8da3dceef720657f762340b941e1a27d
하보영 <@IT->
2022-06-10 15:51:41 +0900

    #... : Detail . useEffect / useCallback?

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,21 +1,43 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
+import Movie from "../components/Movie";
 
 function Detail() {
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState(null);
+
   console.log(id);
 
-  const getMovie = async () => {
+  const getMovie = useCallback(async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
-  };
+
+    setMovie(json.data.movie);
+    setLoading(false);
+  }, [id]);
 
   useEffect(() => {
     getMovie();
-  }, []);
-  return <h1>Detail</h1>;
+  }, [getMovie]);
+
+  console.log(movie);
+
+  return loading ? (
+    <h1>Loading...</h1>
+  ) : (
+    <h1>
+      Detail
+      <Movie
+        coverImg={movie.large_cover_image}
+        summary={movie.description_full}
+        id={movie.id}
+        title={movie.title}
+        genres={movie.genres}
+      />
+    </h1>
+  );
 }
 
 export default Detail;
