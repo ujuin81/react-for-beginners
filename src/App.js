eed39f1b34818f67f29bcf19e92baecda94cc163
하보영 <@IT->
2022-06-10 09:24:12 +0900

    #7.4 Movie App part Two

--- src/App.js
+++ src/App.js
@@ -1,58 +1,5 @@
-import { useEffect, useState } from "react";
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-      )
-    ).json();
-    // const response = await fetch(
-    //   "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-    // );
-    // const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-    // async / await 사용으로 변경
-    // fetch(
-    //   "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-    // )
-    //   .then((response) => response.json())
-    //   .then((json) => {
-    //     setMovies(json.data.movies);
-    //     setLoading(false);
-    //   });
-  }, []);
-
-  console.log(movies);
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((genre) => (
-                  <li key={genre}>{genre}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
